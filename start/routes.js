dd11cdf8cbc115eb97f8add37f461263c4abef42
WagnerWD40 <wagnerwd40@gmail.com>
2020-11-15 17:22:09 -0300

    added password recovey and SMTPconfiguration

--- start/routes.js
+++ start/routes.js
@@ -21,3 +21,8 @@ Route.get('/', () => {
 })
 
 Route.post('users', 'UserController.store')
+
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
